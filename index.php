1973d0fc13e178ff430f7fb694ae793556a9c1e5
saqib khan <saqibadridi464@gmail.com>
2023-08-21 14:34:23 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$abc = 30;
+$xyz = 40;
+
+switch ($abc) {
+    case '30':
+        echo 'its Monday';
+        break;
+    case '40':
+        echo 'its tuesday';
+        break;
+    
+    default:
+        echo 'its holiday';
+}
+?>
